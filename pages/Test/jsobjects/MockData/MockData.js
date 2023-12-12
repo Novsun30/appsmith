28b2a1255b00fed470348f55486ebacd42f3aada
novsun30 <novsun30@gmail.com>
2023-12-12 16:35:25 +0800

    Revert "Revert "add test page""
    This reverts commit d6034dabbc280c0c9887b2d3e8cfdf5f30b9f1b6.

---
+++ pages/Test/jsobjects/MockData/MockData.js
@@ -0,0 +1,55 @@
+export default {
+	data1: [
+		{
+			"display_name": {
+				"additionalProp1": "string",
+				"additionalProp2": "string",
+				"additionalProp3": "string",
+				"additionalProp4": {"test": "test"},
+				"additionalProp5": [{abc:"123", cde:"456"}]
+			},
+			"event_id": "44351904-e",
+			"event_name": "string",
+			"expired_time": "2022-07-27T14:16:00Z",
+			"market_id": "225_56842695",
+			"market_name": "string",
+			"new_odds": 0,
+			"odds_multiplier": 1.03,
+			"old_odds": 0,
+			"product_id": "13",
+			"product_name": "string",
+			"start_time": "2022-07-27T14:16:00Z",
+			"status": "open"
+		},
+			{
+			"display_name": {
+				"additionalProp1": "test",
+				"additionalProp2": "asd",
+				"additionalProp3": "weee",
+				"additionalProp4": {"test": "test"},
+				"additionalProp5": [{abc:"test", cde:"test"}]
+			},
+			"event_id": "40691104-e",
+			"event_name": "string",
+			"expired_time": "2023-08-17T14:05:00Z",
+			"market_id": "304_56842695",
+			"market_name": "string",
+			"new_odds": 0,
+			"odds_multiplier": 1.66,
+			"old_odds": 0,
+			"product_id": "4",
+			"product_name": "string",
+			"start_time": "2022-07-27T14:16:00Z",
+			"status": "open"
+		}
+	],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
