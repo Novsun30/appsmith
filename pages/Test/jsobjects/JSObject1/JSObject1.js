02808585dff7981ff7a1dccb3c6d92aece1d89c3
morris66630 <morris66630@gmail.com>
2023-12-14 02:58:17 +0000

    add mock data table

--- pages/Test/jsobjects/JSObject1/JSObject1.js
+++ pages/Test/jsobjects/JSObject1/JSObject1.js
@@ -1,36 +1,7 @@
 export default {
 	myVar1: [],
-	myVar2: { 
-		"data" : { 
-			"user" : [{ 
-			"name": "Bob",
-		 	"age": 18 , 
-		 	"detail": { 
-				height: "170cm",
-				weight: "60kg"
-			}
-		}, 
-								{ 
-		"name": "Amy", 
-		"age": 22 ,
-		 "detail": { 
-				height: "155cm",
-				weight: "48kg"
-			}, 
-		}, 
-								{ 
-		"name": "Wilson", 
-		"age": 36 ,
-		 "detail": { 
-				height: "170cm",
-				weight: "60kg"
-			}, 
-		}
-		]
-	},
 	myFun1 () {
-		//	write code here
-		//	this.myVar1 = [1,2,3]
+		console.log(this)
 	},
 	async myFun2 () {
 		//	use async-await or promises
